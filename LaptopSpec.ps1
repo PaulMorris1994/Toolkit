d7b428d044bae6152fbbd4ec951c3e8e71a14668
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:37:15 +0000

    Add files via upload

---
+++ LaptopSpec.ps1
@@ -0,0 +1,60 @@
+﻿if (test-path S:\) {
+    $Name = Hostname
+$os = Get-CimInstance -ClassName CIM_OperatingSystem
+$disk = Get-CimInstance -ClassName CIM_LogicalDisk -Filter "DeviceID='C:'"
+$Laptop = Get-CimInstance -ClassName CIM_ComputerSystem
+$Proc = Get-CimInstance -ClassName Win32_Processor
+$RD = Get-CimInstance win32_bios
+$HDD = Get-PhysicalDisk
+
+
+     $Prop=[Ordered]@{
+                'Manufacturer' = $laptop.Manufacturer
+                'Laptop Model' = $laptop.model
+                'RD' = $RD.ReleaseDate
+                'ComputerName' = $laptop.name
+                'Username' = $laptop.username
+                'Serial Number' = $RD.SerialNumber            
+                'OS Name' = $OS.caption
+                'Drive Type' = $HDD.Mediatype
+                'Disk Size C:' = $disk.Size /1gb -as [int]
+                'Memory' = $laptop.TotalPhysicalMemory /1gb -as [int]
+                'Processor' = $Proc.name   
+                'OS Build' = $OS.buildNumber
+                'FreeSpace C:' = $disk.FreeSpace / 1gb -as [int] 
+                }
+                $obj = New-Object -TypeName PSobject -Property $Prop | export-csv "S:\$Name.csv" -NoTypeInformation
+}
+else {
+    $Name = Hostname
+    $os = Get-CimInstance -ClassName CIM_OperatingSystem
+    $disk = Get-CimInstance -ClassName CIM_LogicalDisk -Filter "DeviceID='C:'"
+    $Laptop = Get-CimInstance -ClassName CIM_ComputerSystem
+    $Proc = Get-CimInstance -ClassName Win32_Processor
+    $RD = Get-CimInstance win32_bios
+    $HDD = Get-PhysicalDisk
+
+
+     $Prop=[Ordered]@{
+                'Manufacturer' = $laptop.Manufacturer
+                'Laptop Model' = $laptop.model
+                'RD' = $RD.ReleaseDate
+                'ComputerName' = $laptop.name
+                'Username' = $laptop.username
+                'Serial Number' = $RD.SerialNumber            
+                'OS Name' = $OS.caption
+                'Drive Type' = $HDD.Mediatype
+                'Disk Size C:' = $disk.Size /1gb -as [int]
+                'Memory' = $laptop.TotalPhysicalMemory /1gb -as [int]
+                'Processor' = $Proc.name   
+                'OS Build' = $OS.buildNumber
+                'FreeSpace C:' = $disk.FreeSpace / 1gb -as [int]                                             
+                }
+                $obj = New-Object -TypeName PSobject -Property $Prop | export-csv "$home\documents\$Name.csv" -NoTypeInformation
+}
+
+If (Test-Path "S:\$Name.csv"){
+   Read-Host "Your document is saved in the following location: "S:\$Name.csv"" }
+Else {
+   Read-Host "Your document is saved in the following location: "$home\documents\$Name.csv""
+   }
---
+++ SentEmailCount.ps1
@@ -0,0 +1,8 @@
+﻿Connect-ExchangeOnline
+$start = get-date -Hour 00 -Minute 00 -Second 01
+$End = get-date -Hour 23 -Minute 59 -Second 59
+$User = Read-Host "Type Email address of user you want to search"
+
+Get-MessageTrace -SenderAddress $User -StartDate $start -EndDate $end | Group-Object senderaddress | select name, count | sort Count
+
+Disconnect-ExchangeOnline
