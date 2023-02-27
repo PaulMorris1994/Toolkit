b8eb7fd7a91050f61555242bb8fbed91e0dc887b
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:32:53 +0000

    Add files via upload

---
+++ AddToCase.ps1
@@ -0,0 +1,7 @@
+﻿$Reg = Get-ItemPropertyvalue 'HKCU:\SOFTWARE\Microsoft\Office\Outlook\Addins\sls.OutlookAddin.v2.2.122' -Name Loadbehavior
+
+If (  $Reg -eq "2" ) {
+    Set-ItemProperty 'HKCU:\SOFTWARE\Microsoft\Office\Outlook\Addins\sls.OutlookAddin.v2.2.122' -Name Loadbehavior -Value 3 -Verbose
+    Read-Host "Job completed. Please open Outlook to check add-in."
+    }
+else { Read-Host "Please report add-in issue to itsupport@cartwrightking.co.uk." }
