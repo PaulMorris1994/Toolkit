e09c3ea446d1cf0cb3a118dbc4f301f92d8d5618
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:23:24 +0000

    Add files via upload

---
+++ ACL.ps1
@@ -0,0 +1,9 @@
+﻿$Folder = import-csv -Path "\\sacwkfsuks.file.core.windows.net\shareddrive\paul morris\U Drive\Test_Move.csv"
+
+ForEach ( $F in $Folder ) {
+    Move-Item -Path $F.Path -Destination $F.Destination -Force -Verbose
+    $ACL = Get-Acl "\\sacwkfsuks.file.core.windows.net\shareddrive\paul morris\acl"
+    $fileSystemAccessRule = New-Object -TypeName System.Security.AccessControl.FileSystemAccessRule ("cknotts\$($F.SamAccountName)",'FullControl','ContainerInherit,ObjectInherit','None','Allow') -Verbose
+    $Acl.SetAccessRule($fileSystemAccessRule)
+    Set-ACL -Path $F.ACL -AclObject $ACL -Verbose
+}
