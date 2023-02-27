301ae979d7747c1ab067237ea185adbb6c490926
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:33:33 +0000

    Add files via upload

---
+++ Logon Times.ps1
@@ -0,0 +1,8 @@
+﻿$vm = get-azvm | where { $_.Name -like "*Prod*" } | select Name
+
+ForEach ($V in $VM) {
+Invoke-Command -ComputerName $V.Name -Credential $Cred { get-winevent -LogName "Microsoft-Windows-TerminalServices-LocalSessionManager/Operational" | Where { $_.id -like "21" -or $_.id -like "23" -or $_.id -like "24" -and $_.Message -like "*cknotts\paulm*" } | select-object -property Message, Id, TimeCreated } | export-csv -Path "U:\Paul Morris\Logs\pm.csv" -Append
+}
+
+
+
