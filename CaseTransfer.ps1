915a8ab5b7e4895eba815410412a6da471d4a805
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:31:52 +0000

    Add files via upload

---
+++ CaseTransfer.ps1
@@ -0,0 +1,27 @@
+﻿$Cases = Import-CSV "\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\Care\chetd\cases.csv"
+
+Foreach ($C in $Cases) {
+
+    $Folder = $c.Destination.Split("\")
+    $DestinationTestMatter = [String]::Join('\',$folder[0],$Folder[1],$Folder[2],$Folder[3],$Folder[4])
+    $DestinationTestNumber = [String]::Join('\',$folder[0],$Folder[1],$Folder[2],$Folder[3],$Folder[4],$Folder[5])
+    $MatterFolder = [String]::Join('\',$folder[0],$Folder[1],$Folder[2],$Folder[3])
+    $Matter = $folder[4]
+    $Number = $folder[5]
+
+    If(-Not(Test-Path $DestinationTestMatter)) {
+       New-Item -ItemType Directory -Path $Matterfolder -Name $Matter -Verbose
+       New-Item -ItemType Directory -Path $DestinationTestMatter -Name $Number -Verbose
+       gci -Recurse -Path $C.Path | Copy-Item -Destination $c.Destination -Verbose
+    }
+    ElseIf(-Not(Test-Path $DestinationTestNumber)) {
+       New-Item -ItemType Directory -Path $DestinationTestMatter -Name $Number -Verbose
+       gci -Recurse -Path $C.Path | Copy-Item -Destination $c.Destination -Verbose
+    }
+    Else {
+        Write-Host $C.Path -ForegroundColor red -BackgroundColor Black
+        #gci -Recurse -Path $C.Path | where { $_.LastWriteTime -gt "11/28/2022 11:00:00" } | Copy-Item -Destination $c.Destination -Force -Verbose
+        gci -Recurse -Path $C.Path | where { $_.LastWriteTime -gt "11/28/2022 11:00:00" } | fl FullName, LastWritetime
+    }
+
+}
---
+++ FolderSize.ps1
@@ -0,0 +1,28 @@
+﻿$Cases = Import-csv "\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\LF Purge\Azure\ClosedCases.csv"
+
+ForEach ($C in $Cases) {
+
+If (Test-Path $C.Case) {
+$C.Case
+
+$Size = (gci -Recurse -Path $C.Case | Measure Length -s).Sum /1mb
+
+$Data=[Ordered]@{
+    'Name' = $C.Case
+    'Size(MB)' = $Size
+    }
+}
+Else {
+
+$C.Case
+
+$Data=[Ordered]@{
+    'Name' = $C.Case
+    'Size(MB)' = "No path found"
+    }
+
+}
+
+$Export = New-Object -TypeName PSobject -Property $Data | export-csv "\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\LF Purge\Azure\ClosedCasesData.csv" -NoTypeInformation -Append -verbose
+
+}
