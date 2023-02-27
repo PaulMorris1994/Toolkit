b3148f6e3a90485d552e989ecaf6922212f7c416
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:28:38 +0000

    Add files via upload

---
+++ Adding AD Extension Attribute.ps1
@@ -0,0 +1,12 @@
+﻿$User = Get-ADUser -Filter * | where { $_.DistinguishedName -Like "*worcester*" } | Select DistinguishedName, Givenname, Surname
+
+ForEach ($U in $User) {
+Set-ADUser -Identity $U.DistinguishedName -Add @{ ExtensionAttribute1=$U.GivenName + '.' + $U.Surname +"@cartwrightking.co.uk.cjsm.net" }
+    }
+
+
+$User = Get-ADUser -Filter * | where { $_.DistinguishedName -Like "*nottingham*" } | Select DistinguishedName, Givenname, Surname
+
+ForEach ($U in $User) {
+Get-ADUser -Identity $U.DistinguishedName -Properties * | select name, ExtensionAttribute1
+    }
---
+++ MobileNumber.ps1
@@ -0,0 +1,14 @@
+﻿$Num = Import-Csv "C:\Users\ckadmin\Documents\Number.csv"
+
+ForEach ($n in $num) {
+    
+    $SAN = (Get-aduser -Filter " Name -like '$($N.Name)' ").SamAccountName
+    If ( Get-aduser -Filter " Name -like '$($N.Name)' " )
+        {
+            Set-ADUser -Identity $SAN -MobilePhone $N.Number
+        }
+    Else {
+            Write-Warning "$($N.Name) Does not exsist" 
+        }
+
+}
