5ed9766db4d379dfe13d9b21bed84b1b0f1fbddd
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:20:20 +0000

    Delete AzureFileShare.ps1

--- AzureFileShare.ps1
+++
@@ -1,5 +0,0 @@
-if(-not(get-psdrive -name M)) { cmd.exe /C "cmdkey /add:`"pulmorrisltdstorage.file.core.windows.net`" /user:`"localhost\pulmorrisltdstorage`" /pass:`"Y1rsUUbzZJ5wuxs3r9P9OhoT6V1+43+Z3Z69dHOmgMIWTzJRlciuD7qah2WDJ24EeW72NTN1Xd0E+AStOaZtkQ==`""
-    # Mount the drive
-    New-PSDrive -Name f -PSProvider FileSystem -Root "\\pulmorrisltdstorage.file.core.windows.net\shared-storage" -Persist 
-    }
-    
