c30175b58489544b684173026b11b6bcc9c15bb8
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-02-27 16:37:29 +0000

    Add files via upload

---
+++ EmailNotification.ps1
@@ -0,0 +1,25 @@
+﻿$Doc = gci -Path "\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\test.txt" | select LastwriteTime
+$Document = '"\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\test.txt"'
+$Date = (Get-Date).AddDays(-1)
+$Cred = Get-StoredCredential -Target automation
+
+$Email = @{ 
+SMTPServer = "smtp.office365.com"
+To         = "starters-leavers@cartwrightking.co.uk"
+From       = "itsupport@cartwrightking.co.uk"
+Credential = $Cred
+Port       = "587"
+UseSSL     = $True
+Subject    = "Starter & Leaver Update Notification"
+Body       = "Hi All
+
+Please follow the link below for the latest updates.
+    
+Document: $Document
+    
+Many thanks"
+}
+
+If ( $Doc.Lastwritetime -gt $date ) { 
+    Send-MailMessage @Email
+}
