8c9968b135eb2f6ffb7ec3c99801ba1c1fdfd115
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-03-03 11:57:24 +0000

    Add files via upload

---
+++ AVDUserHost.ps1
@@ -0,0 +1,7 @@
+﻿Connect-AzAccount -Subscription 9d8f1854-faac-4ad0-a1cf-6d71afc17001
+$HP = "hp-avd-prod-uks"
+$RG = "rg-avd-prod-uks"
+$User = Read-Host "Type username"
+
+Get-AzWvdUserSession -ResourceGroupName $RG -HostPoolName $HP | Where { $_.ActiveDirectoryUserName -like "*$User*" } | select ActiveDirectoryUserName, Name
+
