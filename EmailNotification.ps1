35cc327be7eb65c9193ae1953b26f84d49d378b7
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-03-03 09:34:25 +0000

    Update EmailNotification.ps1

--- EmailNotification.ps1
+++ EmailNotification.ps1
@@ -1,12 +1,13 @@
-﻿$Doc = gci -Path "\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\test.txt" | select LastwriteTime
-$Document = '"\\sacwkfsuks.file.core.windows.net\shareddrive\Paul Morris\test.txt"'
+import-module CredentialManager -Force
+$Doc = gci -Path "\\sacwkfsuks.file.core.windows.net\shareddrive\_StartersLeavers\RD-REP-Starters and Leavers Tracker Oct 2021 v1_00.xlsx" | select LastwriteTime
+$Document = '"\\sacwkfsuks.file.core.windows.net\shareddrive\_StartersLeavers\RD-REP-Starters and Leavers Tracker Oct 2021 v1_00.xlsx"'
 $Date = (Get-Date).AddDays(-1)
 $Cred = Get-StoredCredential -Target automation
 
 $Email = @{ 
 SMTPServer = "smtp.office365.com"
-To         = "starters-leavers@cartwrightking.co.uk"
-From       = "itsupport@cartwrightking.co.uk"
+To         = "starters-leavers@cartwrightking365.onmicrosoft.com"
+From       = "Personnel@cartwrightking.co.uk"
 Credential = $Cred
 Port       = "587"
 UseSSL     = $True
@@ -22,4 +23,11 @@ Many thanks"
 
 If ( $Doc.Lastwritetime -gt $date ) { 
     Send-MailMessage @Email
+}
+
+$Test = Test-Path "\\sacwkfsuks.file.core.windows.net\shareddrive\_StartersLeavers\RD-REP-Starters and Leavers Tracker Oct 2021 v1_00.xlsx"
+
+If ($test.Equals($False)) {
+        Send-MailMessage -Credential $Cred -Port 587 -UseSsl -smtpserver smtp.office365.com -To Paul.Morris@cartwrightking.co.uk -From Paul.Morris@cartwrightking.co.uk -Subject "Starter & Leaver doc" -Body "RD-REP-Starters and Leavers Tracker Oct 2021 v1_00.xlsx has moved / changed name"
+
 }
