3936d0b8167322ffb1f3c39b8179265fc0d9e999
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-03-06 12:43:01 +0000

    Update New Starter_v3.ps1

--- New Starter_v3.ps1
+++ New Starter_v3.ps1
@@ -1,4 +1,4 @@
-﻿$AdminCred = Get-StoredCredential -Target admin
+$AdminCred = Get-StoredCredential -Target admin
 $Session = New-PSSession -ComputerName CKSAZAD002 -Credential $Admincred
 
 Invoke-Command -Session $Session { 
@@ -9,7 +9,7 @@ $MobilePhone = read-host "Please type users work mobile number"
 $OfficePhone = read-host "Please type this users landline number"
 $Password = read-host "Please type users password"
 $Office = read-host "Please type the office the user will be based at"
-$Department = read-host "Please type the department this user will work in:  Care | Chambers | Court of Protection | Crime | Family | Fraud | HR | Immigration | Marketing | Training"
+$Department = read-host "Please type the department this user will work in"
 $title = read-host "Please type the users job title"
 $EmployeeType = read-host "Employee or Consultant"
 $Emailaddress = $Givenname + "." + $Surname +"@cartwrightking.co.uk"
@@ -40,6 +40,7 @@ If (-Not((Get-ADUser -Filter "Name -like '$($Name)'"))) {
     Add-ADGroupMember -Members $SamAccountName -Identity "AAD-Lic-MSBusPrem" -Verbose
     $DistinguishedName = (Get-ADUser -identity $SamAccountName -Properties *).DistinguishedName
     Set-ADUser -Identity $DistinguishedName -Add @{ ExtensionAttribute1=$GivenName + '.' + $Surname +"@cartwrightking.co.uk.cjsm.net" } -Verbose
+    set-ADUser -Identity $DistinguishedName -Add @{ProxyAddresses="SMTP:$EmailAddress"}
     New-Item -ItemType Directory -Name $Name -Path "\\sacwkfsuks.file.core.windows.net\shareddrive" -Verbose
 
     $NewUserExport = [Ordered]@{
@@ -63,14 +64,23 @@ else {
 If ($Department -like "Crime") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Crime" -Verbose
 }
-ElseIf ($Department -like "Court of Protection") {
-Add-ADGroupMember -Members $SamAccountName -Identity "All.COP" -Verbose
+ElseIf ($Department -like "Accounts") {
+Add-ADGroupMember -Members $SamAccountName -Identity "FinanceTeam" -Verbose
 }
 ElseIf ($Department -like "Compliance") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Compliance" -Verbose
 }
+ElseIf ($Department -like "Conveyancing") {
+Add-ADGroupMember -Members $SamAccountName -Identity "All.Conveyancing" -Verbose
+}
 ElseIf ($Department -like "Chambers") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Chambers" -Verbose
+}
+ElseIf ($Department -like "Employment") {
+Add-ADGroupMember -Members $SamAccountName -Identity "All.Employment" -Verbose
+}
+ElseIf ($Department -like "Family") {
+Add-ADGroupMember -Members $SamAccountName -Identity "All.Family" -Verbose
 }
 ElseIf ($Department -like "Care") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Care" -Verbose
@@ -83,9 +93,6 @@ Add-ADGroupMember -Members $SamAccountName -Identity "All.HR" -Verbose
 }
 ElseIf ($Department -like "Fraud") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Fraud" -Verbose
-}
-ElseIf ($Department -like "Chambers") {
-Add-ADGroupMember -Members $SamAccountName -Identity "All.Chambers" -Verbose
 }
 ElseIf ($Department -like "Mental Health") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.MentalHealth" -Verbose
@@ -103,57 +110,46 @@ Write-Warning "This user hasnt been added into any Department groups as the depa
 If ($Office -like "Birmingham") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Birmingham" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_Birmingham_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "The McLaren Building, 46 The Priory Queensway" -PostalCode "B4 7LR" -City "Birmingham" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "Lewis Building, Bull Street," -PostalCode "B4 6AF" -City "Birmingham" -Verbose
 Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Birmingham,DC=cknotts,DC=co,DC=uk" -Verbose
 }
-ElseIf ($Office -like "Bedford") {
-Add-ADGroupMember -Members $SamAccountName -Identity "All.Bedford" -Verbose
-Add-ADGroupMember -Members $SamAccountName -Identity "SG_Bedford_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "Clifton House, 4a Goldington Road" -PostalCode "MK40 3NF" -City "Bedford" -Verbose
-Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Bedford,DC=cknotts,DC=co,DC=uk" -Verbose
+ElseIf ($Office -like "Blackburn") {
+Add-ADGroupMember -Members $SamAccountName -Identity "All.Blackburn" -Verbose
+Add-ADGroupMember -Members $SamAccountName -Identity "SG_Blackburn_office" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "First Floor, One Cathedral Square," -PostalCode "BB1 1FB" -City "Blackburn," -Verbose
+Move-ADObject -Identity "$DistinguishedName" -TargetPath "OU=Users,OU=Blackburn,DC=cknotts,DC=co,DC=uk" -Verbose
 }
 ElseIf ($Office -like "Derby") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Derby" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_Derby_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "Peat House, 5 Stuart Street" -PostalCode "DE1 2EQ" -City "Derby" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "The Old Post Office, Victoria Street," -PostalCode "DE1 1EQ" -City "Derby" -Verbose
 Move-ADObject -Identity "$DistinguishedName" -TargetPath "OU=Users,OU=Derby,DC=cknotts,DC=co,DC=uk" -Verbose
-}
-ElseIf ($Office -like "Leeds") {
-Add-ADGroupMember -Members $SamAccountName -Identity "All.Leeds" -Verbose
-Add-ADGroupMember -Members $SamAccountName -Identity "SG_Leeds_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "3rd Floor, St Paul's House, 23 Park Square South" -PostalCode "LS1 2ND" -City "Leeds" -Verbose
-Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Leeds,DC=cknotts,DC=co,DC=uk" -Verbose
 }
 ElseIf ($Office -like "Nottingham") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Nottingham" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_Nottingham_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "Lock House, Wilford Street" -PostalCode "NG2 1AG" -City "Nottingham" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "6th Floor City Gate East, Toll House Hill," -PostalCode "NG1 5FS" -City "Nottingham" -Verbose
 Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Nottingham full internet - Lawfusion,DC=cknotts,DC=co,DC=uk" -Verbose
 }
 ElseIf ($Office -like "Manchester") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Manchester" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_Manchester_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "Grampian House, Deansgate" -PostalCode "M3 3EE" -City "Manchester" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "82 King Street," -PostalCode "M2 4WQ" -City "Manchester" -Verbose
 Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Manchester,DC=cknotts,DC=co,DC=uk" -Verbose
 }
 ElseIf ($Office -like "London") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.London" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_London_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "5th Floor, Eldon Chambers, 30-32 Fleet Street" -PostalCode "EC4Y 1AA" -City "London" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "1 Trafalgar Square," -PostalCode "WC2N 5BW" -City "London" -Verbose
 Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=London,DC=cknotts,DC=co,DC=uk" -Verbose
 }
 ElseIf ($Office -like "Leicester") {
 Add-ADGroupMember -Members $SamAccountName -Identity "All.Leicester" -Verbose
 Add-ADGroupMember -Members $SamAccountName -Identity "SG_Leicester_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "Horsefair House, 3 Horsefair Street" -PostalCode "LE1 5BP" -City "Leicester" -Verbose
+Set-ADUser -Identity $DistinguishedName -StreetAddress "St. George's House, 6 St George's Way," -PostalCode "LE1 1QZ" -City "Leicester" -Verbose
 Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Leicester,DC=cknotts,DC=co,DC=uk" -Verbose
 }
-ElseIf ($Office -like "Sheffield") {
-Add-ADGroupMember -Members $SamAccountName -Identity "All.Sheffield" -Verbose
-Add-ADGroupMember -Members $SamAccountName -Identity "SG_Sheffield_office" -Verbose
-Set-ADUser -Identity $DistinguishedName -StreetAddress "St. James House, Vicar Lane" -PostalCode "S1 2EX" -City "Sheffield" -Verbose
-Move-ADObject -Identity $DistinguishedName -TargetPath "OU=Users,OU=Sheffield,DC=cknotts,DC=co,DC=uk" -Verbose
-}
+
 else {
 Write-Warning "This user hasnt been added into any Office groups as the office text entered does not match any of the groups.  Please add this user into the relevant groups manually."
 }
@@ -162,7 +158,10 @@ $User = Get-ADUser -Identity $SamAccountName -Properties * | Select Name, EmailA
 
 If ($User.Description -like "Consultant") {
     $DN = (Get-ADUser -identity $SamAccountName -Properties *).DistinguishedName
-    Remove-ADGroupMember -Members $SamAccountName -Identity "Cartwright King" -Confirm:$False -Verbose
+    $Groups = Get-ADPrincipalGroupMembership -Identity $SAN | where { $_.Name -notlike "Domain Users" -and $_.Name -notlike "AAD-Lic-MSBusPrem" -and $_.Name -notlike "Azure" } | select Name
+    ForEach ($G in $Groups) {
+        Remove-ADPrincipalGroupMembership -Identity $SAN -MemberOf $G.Name -Confirm:$False -Verbose
+    }
     Add-ADGroupMember -Members $SamAccountName -Identity "Consultants" -Verbose
     Move-ADObject -Identity $DN -TargetPath "OU=Consultants,DC=cknotts,DC=co,DC=uk" -Verbose
 }
