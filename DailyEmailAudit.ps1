eae451f25e0e4f74830df3943329dad917846dbf
PaulMorris1994 <50925796+PaulMorris1994@users.noreply.github.com>
2023-03-24 16:08:05 +0000

    Add files via upload

---
+++ DailyEmailAudit.ps1
@@ -0,0 +1,13 @@
+﻿#Get-MailboxPermission -Identity accounts | ft Identity, User, accessrights -AutoSize
+
+$Sday = Get-Date -Hour 00 -Minute 00 -Second 01
+$Eday = get-date -Hour 23 -Minute 59 -Second 59
+$Sender = Read-Host "Type users email address for list of emails or type count for the total"
+
+If($Sender -eq "Count") {
+    $Sender = Read-Host "Type users email address"
+    (Get-MessageTrace -SenderAddress $Sender -StartDate $Sday -EndDate $Eday).Count
+        }
+Else {
+    Get-MessageTrace -SenderAddress $Sender -StartDate $Sday -EndDate $Eday | FT SenderAddress, RecipientAddress, Subject, Status, Received -AutoSize             
+        }
